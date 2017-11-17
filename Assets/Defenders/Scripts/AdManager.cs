8a0936552d6c4e273998cd7e8bbd8b49627c9f16
Liu <liu170045@gmail.com>
2017-11-17 11:05:58 +0800

    修改结束panel

--- Assets/Defenders/Scripts/AdManager.cs
+++ Assets/Defenders/Scripts/AdManager.cs
@@ -46,8 +46,8 @@ public class AdManager : MonoBehaviour
         //ad.setTesting(true);
         Debug.Log("Admob Inited.");
 
-        //showBannerAd (always)
-        Admob.Instance().showBannerRelative(AdSize.Banner, AdPosition.BOTTOM_CENTER, 0);
+        ////showBannerAd (always)
+        //Admob.Instance().showBannerRelative(AdSize.Banner, AdPosition.BOTTOM_CENTER, 0);
 
         //cache an Interstitial ad for later use
         ad.loadInterstitial();
